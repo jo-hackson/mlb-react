206a5e8bd9190b96836adc4ab644ac867e86e270
Joanna <joanna.yeh.jackson@gmail.com>
2017-12-13 14:26:53 -0800

    Update Redux state with email and token when user login

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,13 +1,26 @@
 // functional component
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const HomePage = () => (
+
+const HomePage = ({ isAuthenticated }) => (
 	<div>
 		<h1>Welcome to My Little Banker</h1>
-		<p><Link to="/login">login</Link> or <Link to="/signup">sign up</Link></p>
+		{ isAuthenticated ? <button>logout</button> : <p><Link to="/login">login</Link> or <Link to="/signup">sign up</Link></p> }
   </div>
 );
 
-export default HomePage;
+HomePage.propTypes = {
+	isAuthenticated: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state) {
+	return {
+		isAuthenticated: !!state.user.token
+	}
+}
+
+export default connect(mapStateToProps)(HomePage);
