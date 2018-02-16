a859fe04518c469793e8d7925a339f8150722082
Joanna <joanna.yeh.jackson@gmail.com>
2018-02-16 15:25:42 -0600

    Add home and logout button on dashboard page

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
+import * as actions from '../../actions/auth';
+import { Button } from 'semantic-ui-react';
 // import ConfirmEmailMessage from '../messages/ConfirmEmailMessage';
 // import AddChildForm from '../forms/AddChildForm';
 // import { addChild } from '../../actions/parents';
@@ -8,12 +11,13 @@ import { connect } from 'react-redux';
 class DashboardPage extends React.Component {
 
 	render() {
-		const { gender, lastName, isMale } = this.props;
+		const { gender, lastName, isMale, logout } = this.props;
 		return (
 			<div>
+				<Link to="/"><i className="circular home icon"></i></Link>
+				<Button className="circular basic icon" onClick={() => logout()}><i className="circular log out icon"></i></Button>
 				<h1>dashboard page</h1>
 				<h2>{ isMale ? <span>Mr.</span> : <span>Mrs.</span> } { lastName } </h2>
-
 			</div>
 		);
 	};
@@ -21,7 +25,8 @@ class DashboardPage extends React.Component {
 
 DashboardPage.propTypes = {
 	gender: PropTypes.string,
-	lastName: PropTypes.string
+	lastName: PropTypes.string,
+	logout: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state) {
@@ -33,4 +38,4 @@ function mapStateToProps(state) {
 };
 
 
-export default connect(mapStateToProps)(DashboardPage);
+export default connect(mapStateToProps, { logout: actions.logout })(DashboardPage);
